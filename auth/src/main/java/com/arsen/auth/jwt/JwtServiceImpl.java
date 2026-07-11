02b280a7e9ef862abc0c9490cde7c342c1e7fd4c
arsen-R <arsenr412@gmail.com>
2026-07-11 15:12:26 +0100

    added JwtService & JwtServiceImpl

---
+++ auth/src/main/java/com/arsen/auth/jwt/JwtServiceImpl.java
@@ -0,0 +1,86 @@
+package com.arsen.auth.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+    @Value("${application.security.jwt.secret-key}")
+    private String JWT_SECRET_KEY;
+    @Value("${application.security.jwt.expiration}")
+    private Long JWT_EXPIRATION;
+
+    private static final String ROLES = "roles";
+    private static final String USERNAME = "username";
+    private static final String IS_ENABLED = "isEnabled";
+    private static final String IS_ACCOUNT_NON_LOCKED = "isAccountNonLocked";
+
+    @Override
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    @Override
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    @Override
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    @Override
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private String createToken(Map<String, Object> claims, UserDetails userDetails) {
+        return Jwts.builder().setClaims(claims)
+                .claim(ROLES, userDetails.getAuthorities())
+                .claim(USERNAME, userDetails.getUsername())
+                .claim(IS_ENABLED, userDetails.isEnabled())
+                .claim(IS_ACCOUNT_NON_LOCKED, userDetails.isAccountNonLocked())
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+    private Key getSecretKey() {
+        byte[] keyBytes = Base64.getDecoder().decode(JWT_SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
