7ffb7b65c1e141f226f599c7fc253c6810d64902
arsen-R <arsenr412@gmail.com>
2026-07-20 21:47:45 +0100

    Added config-server module

--- config-server/src/main/java/com/arsen/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/arsen/configserver/ConfigServerApplication.java
@@ -1,15 +1,17 @@
-package com.arsen.auth;
+package com.arsen.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class AuthApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
